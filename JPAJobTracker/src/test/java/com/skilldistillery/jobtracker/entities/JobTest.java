248c4768e8dff76b815d262762dac5c3181373c1
arpapp <trpapp@gmail.com>
2020-07-14 10:38:31 -0600

    all tests passed for entities

--- JPAJobTracker/src/test/java/com/skilldistillery/jobtracker/entities/JobTest.java
+++ JPAJobTracker/src/test/java/com/skilldistillery/jobtracker/entities/JobTest.java
@@ -1,14 +1,64 @@
 package com.skilldistillery.jobtracker.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class JobTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Job job;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JobTrackerPU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		job = em.find(Job.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		job = null;
+		em.close();
+	}
+
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		assertNotNull(job);
+		assertEquals("Software Developer", job.getTitle());
+		assertEquals("Woodridge Software", job.getCompany());
+		assertEquals("Golden, CO", job.getLocation());
+		assertTrue(job.isCoverLetterSubmitted());
+		assertEquals("Kaj Gronholm", job.getContactPerson());
+		assertEquals(null, job.getNotes());
+		assertEquals(Status.PENDING, job.getStatus());
+	}
+	
+	@Test
+	void test_relationship_mapping_User() {
+		assertNotNull(job);
+		assertEquals("Toni", job.getUser().getFirstName());
 	}
 
 }
--- JPAJobTracker/src/test/java/com/skilldistillery/jobtracker/entities/UserTest.java
+++ JPAJobTracker/src/test/java/com/skilldistillery/jobtracker/entities/UserTest.java
@@ -1,14 +1,61 @@
 package com.skilldistillery.jobtracker.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UserTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private User user;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JobTrackerPU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		user = em.find(User.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		user = null;
+		em.close();
+	}
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		assertNotNull(user);
+		assertEquals("tpapp", user.getUsername());
+		assertEquals("Toni", user.getFirstName());
+		assertTrue(user.isEnabled());
+	}
+	
+	@Test
+	void test_relationship_mapping_jobs() {
+		assertNotNull(user);
+		assertTrue(user.getJobs().size() > 0);
+		assertEquals("Software Developer", user.getJobs().get(0).getTitle());
+		
 	}
 
 }
